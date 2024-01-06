51a8e99951f6923706cbab7129c5c193453a32f5
Mirra Petrova <137895634+MirraPetrova@users.noreply.github.com>
2024-01-07 00:12:47 +0300

    Update Parent.cpp

--- Operating Systems(3 sem)/lab2-OS/Parent.cpp
+++ Operating Systems(3 sem)/lab2-OS/Parent.cpp
@@ -1,53 +1,56 @@
 #include <iostream>
 #include <windows.h>
-#include<string>
-using namespace std;
+#include <string>
 
 int main()
 {
     int size;
-    cout << "Enter array size: ";
-    cin >> size;
-    long* arr = new long[size];
-    cout << "Enter array elements: ";
-    for (int i = 0; i < size; i++)
-        cin >> arr[i];
+    std::cout << "Enter array size: ";
+    std::cin >> size;
+    if (size <= 0)
+        std::cout << "Error: uncorrect data";
+    else {
+        long* arr = new long[size];
+        std::cout << "Enter array elements: ";
+        for (int i = 0; i < size; i++)
+            std::cin >> arr[i];
 
-    int X;
-    cout << "Enter X(width): ";
-    cin >> X;
+        int X;
+        std::cout << "Enter X(width): ";
+        std::cin >> X;
 
-    string cmdLine = "Child.exe ";
-    cmdLine += to_string(size) + " ";
-    for (int i = 0; i < size; i++)
-        cmdLine += to_string(arr[i]) + " ";
-    //cmdLine += to_string(X) + " ";
-   
-    int len = cmdLine.length();
-    char* cmdLineChar = new char[len + 1];
-    strcpy_s(cmdLineChar, len + 1, cmdLine.c_str());
+        std::string cmdLine = "Child.exe ";
+        cmdLine += std::to_string(size) + " ";
+        for (int i = 0; i < size; i++)
+            cmdLine += std::to_string(arr[i]) + " ";
+        //cmdLine += std::to_string(X) + " ";
 
-    STARTUPINFO si;
-    ZeroMemory(&si, sizeof(si));
-    si.cb = sizeof(si);
-    si.dwFlags = STARTF_USEPOSITION;
-    si.dwX = X;
+        int len = cmdLine.length();
+        char* cmdLineChar = new char[len + 1];
+        strcpy_s(cmdLineChar, len + 1, cmdLine.c_str()); //copying one line to another
 
-    PROCESS_INFORMATION pi;
-    ZeroMemory(&pi, sizeof(pi));
-    if (!CreateProcess(NULL, cmdLineChar, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
-    {
-        cout << "Error creating child process: " << GetLastError() << endl;
-        return 1;
+        STARTUPINFO si;
+        ZeroMemory(&si, sizeof(si));
+        si.cb = sizeof(si);
+        si.dwFlags = STARTF_USEPOSITION;
+        si.dwX = X;
+
+        PROCESS_INFORMATION pi;
+        ZeroMemory(&pi, sizeof(pi));
+        if (!CreateProcess(NULL, cmdLineChar, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+        {
+            std::cout << "Error creating child process: " << GetLastError() << std::endl;
+            return 1;
+        }
+
+        WaitForSingleObject(pi.hProcess, INFINITE);
+
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+
+        delete[] cmdLineChar;
+        delete[] arr;
     }
-
-    WaitForSingleObject(pi.hProcess, INFINITE);
-
-    CloseHandle(pi.hProcess);
-    CloseHandle(pi.hThread);
-
-    delete[] arr;
-    delete[] cmdLineChar;
 
     return 0;
 }
