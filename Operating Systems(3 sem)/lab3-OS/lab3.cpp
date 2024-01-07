f8be7ac372ad96bf2e70fd6e17e0b79cabf7adaa
Mirra Petrova <137895634+MirraPetrova@users.noreply.github.com>
2024-01-07 03:02:58 +0300

    Add files via upload

--- Operating Systems(3 sem)/lab3-OS/lab3.cpp
+++ Operating Systems(3 sem)/lab3-OS/lab3.cpp
@@ -1,88 +1,123 @@
-#include <iostream>
-#include <thread>
+п»ї#include <iostream>п»ї
 #include <windows.h>
-#include <string>
-using namespace std;
+#include <vector>
+#include <process.h>
 
-HANDLE hEvent;
+HANDLE hEvent1 = CreateEvent(NULL, FALSE, FALSE, NULL);
+HANDLE hEvent2 = CreateEvent(NULL, FALSE, FALSE, NULL);
 CRITICAL_SECTION cs;
+int k;
+int A;
+int interval;
+long long mult = 1;
 
-void workThread(char* arr, int size) {
-    int interval;
-    cout << "Введите временной интервал для отдыха после подготовки одного элемента в массиве: ";
-    cin >> interval;
+DWORD WINAPI Thread2Actions(LPVOID param) {
+	std::vector<double>& arr = *static_cast<std::vector<double>*>(param);
+	std::cout << "Enter a time interval to rest after preparing one element in the array: ";
+	std::cin >> interval;
 
-    //cout << "Enter by thread work" << endl;
-    for (int i = 0; i < size; i++) {
-        if (isdigit(arr[i])) {
-            arr[i] = arr[i];
-        }
-        else {
-            arr[i] = '_';
-        }
-        Sleep(interval);
-    }
+	std::vector<double> rightside;
+	std::vector<double> leftside;
+	for (double number : arr) {
+		if (number < A)
+			rightside.push_back(number);
+		else
+			leftside.push_back(number);
+	}
+	arr.clear();
 
-    SetEvent(hEvent);
+	arr.insert(arr.end(), leftside.begin(), leftside.end());
+	arr.insert(arr.end(), rightside.begin(), rightside.end());
+
+	std::cout << "Forming result array: ";
+	for (int i = 0; i < arr.size(); i++) {
+		std::cout << arr[i] << " ";
+		Sleep(interval);
+	}
+	std::cout << std::endl;
+
+	SetEvent(hEvent1);
+	return 0;
 }
 
-void countElementThread(char* arr, int size,int *count) {
-    EnterCriticalSection(&cs);
-    //cout << "Enter by thread countelement" << endl;
+DWORD WINAPI Thread3Actions(LPVOID param) {
+	std::vector<double>& arr = *static_cast<std::vector<double>*>(param);
+	EnterCriticalSection(&cs);
 
-    WaitForSingleObject(hEvent, INFINITE);
+	WaitForSingleObject(hEvent2, INFINITE);
+	for (size_t i = k; i < arr.size(); i++)
+		mult *= arr[i];
 
-    for (int i = 0; i < size; i++) {
-        if (arr[i] != '_') {
-            (*count)++;
-        }
-    }
-    LeaveCriticalSection(&cs);
+	LeaveCriticalSection(&cs);
+	return 0;
 }
 
 int main() {
-    setlocale(LC_ALL, "rus");
-    int size;
-    cout << "Введите размерность массива: ";
-    cin >> size;
-    char* arr = new char[size];
-    cout << "Заполните массив элементами:" << endl;
-    for (int i = 0; i < size; i++) {
-        cin >> arr[i];
-    }
-    cout << "Исходный массив:" << endl;
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+	A = 0;
+	k = 0;
+	
+	int size;
+	std::cout << "Enter array size: ";
+	std::cin >> size;
+	if (size <= 0)
+		std::cout << "Error: uncorrect data";
+	else {
+		std::vector<double> arr;
+		std::cout << "Enter array elements: " << std::endl;
+		for (int i = 0; i < size; i++) {
+			double number;
+			std::cin >> number;
+			arr.push_back(number);
+		}
 
+		std::cout << std::endl;
+		std::cout << "Array size: " << size << std::endl;
+		std::cout << "Current array: " << std::endl;
+		for (int i = 0; i < size; i++)
+			std::cout << arr[i] << " ";
+		std::cout << std::endl;
 
-    hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-    InitializeCriticalSection(&cs);
+		InitializeCriticalSection(&cs);
 
-    int* count;
-    count = new int[1];
-    *count = 0;
-    thread work(workThread, arr, size);
-    thread countElement(countElementThread, arr, size, count);
+		HANDLE hThread2 = CreateThread(NULL, 0, Thread2Actions, &arr, CREATE_SUSPENDED, NULL);
+		if (hThread2 == NULL)
+			return GetLastError();
+		std::cout << std::endl;
+		std::cout << "Enter A: ";
+		std::cin >> A;
+		std::cout << "Enter k: ";
+		std::cin >> k;
+		if (k >= size)
+			std::cout << "Error: this position does not exist";
+		else {
+			std::cout << std::endl;
+			ResumeThread(hThread2);
 
-    WaitForSingleObject(hEvent, INFINITE);
-    cout << "Итоговый массив:" << endl;
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+			HANDLE hThread3 = CreateThread(NULL, 0, Thread3Actions, &arr, 0, NULL);
+			if (hThread3 == NULL)
+				return GetLastError();
+			WaitForSingleObject(hEvent1, INFINITE);
+			std::cout << std::endl;
+			std::cout << "Result array: ";
+			for (int i = 0; i < size; i++)
+				std::cout << arr[i] << " ";
+			std::cout << std::endl;
 
-    EnterCriticalSection(&cs);
-    cout << "Результат работы потока CountElement: " << *count << endl;
-    LeaveCriticalSection(&cs);
-    
-    work.join();
-    countElement.join();
+			SetEvent(hEvent2);
 
-    CloseHandle(hEvent);
-    DeleteCriticalSection(&cs);
-    delete[] arr;
+			EnterCriticalSection(&cs);
+			std::cout << "Working result of thread MultElement: " << mult << std::endl;
+			LeaveCriticalSection(&cs);
 
-    return 0;
+			WaitForSingleObject(hThread3, INFINITE);
+
+			CloseHandle(hThread3);
+			CloseHandle(hThread2);
+			DeleteCriticalSection(&cs);
+			CloseHandle(hEvent1);
+			CloseHandle(hEvent2);
+		}
+	}
+
+	return 0;
 }
