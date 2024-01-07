675d4fd990ee438aa9b44cd009fb9eeac07cd5e2
Mirra Petrova <137895634+MirraPetrova@users.noreply.github.com>
2024-01-07 15:37:19 +0300

    98 standard

---
+++ Operating Systems(3 sem)/lab4-OS/Child.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <time.h>
+#include <string>
+
+HANDLE hSemaphore, eventC, eventD, EndChild;
+CRITICAL_SECTION csA;
+
+int main(int argc, char* argv[]) {
+
+	hSemaphore = OpenSemaphore(SEMAPHORE_ALL_ACCESS, FALSE, L"Semaphore");
+	if (hSemaphore == NULL) {
+		std::cout << "Create semaphore failed." << std::endl;
+		return GetLastError();
+	}
+	eventC = CreateEvent(NULL, FALSE, FALSE, L"eventC");
+	if (eventC == NULL)
+		return GetLastError();
+	eventD = CreateEvent(NULL, FALSE, FALSE, L"eventD");
+	if (eventD == NULL)
+		return GetLastError();
+	EndChild = CreateEvent(NULL, FALSE, FALSE, L"EndChild");
+	if (EndChild == NULL)
+		return GetLastError();
+
+	int AmountMess = atoi(argv[argc - 1]);//copy the last command line argument in integer
+
+	WaitForSingleObject(hSemaphore, INFINITE);//wait for the semaphore to become signaled
+
+	for (int i = 0; i < AmountMess; i++) {
+		std::cout << "Enter the message(C or D): ";
+		char mess;
+		std::cin >> mess;
+		std::cout << std::endl;
+
+		if (mess == 'C')
+			SetEvent(eventC);
+		if (mess == 'D')
+			SetEvent(eventD);
+
+		SetEvent(EndChild);//signal of ending
+	}
+
+	ReleaseSemaphore(hSemaphore, 1, NULL);
+
+	CloseHandle(EndChild);
+	CloseHandle(eventC);
+	CloseHandle(eventD);
+
+	return 0;
+}
