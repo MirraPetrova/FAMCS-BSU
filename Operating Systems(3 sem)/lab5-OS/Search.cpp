cd96feac16e8d4c2c19f55801c1853f1c0181b86
Mirra Petrova <137895634+MirraPetrova@users.noreply.github.com>
2024-01-07 16:45:24 +0300

    Update Search.cpp

--- Operating Systems(3 sem)/lab5-OS/Search.cpp
+++ Operating Systems(3 sem)/lab5-OS/Search.cpp
@@ -4,65 +4,64 @@
 #include <set>
 #include <vector>
 #include <string>
-using namespace std;
 
 int main(int argc, char* argv[]) {
 
 	HANDLE hWritePipe = (HANDLE)atoi(argv[0]);
 	HANDLE hReadPipe = (HANDLE)atoi(argv[1]);
-	HANDLE hEnableReadServer = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"EnableReadServer");// для синхронизации обмена данными
-	HANDLE hEnableReadSearch = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"EnableReadSearch");// для синхронизации обмена данными
-	if (hEnableReadSearch == NULL || hEnableReadServer == NULL) {// открываем событие, разрешающее чтение
-		cout << "Events were not created";
+	HANDLE hEnableReadServer = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"EnableReadServer");// to synchronize data exchange
+	HANDLE hEnableReadSearch = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"EnableReadSearch");// to synchronize data exchange
+	if (hEnableReadSearch == NULL || hEnableReadServer == NULL) {// opening an event that allows reading
+		std::cout << "Events were not created";
 		return GetLastError();
 	}
 
 	int size1, size2;
 	DWORD bytesWritten;
 
-	WaitForSingleObject(hEnableReadSearch, INFINITE);// ждем разрешения на чтение
+	WaitForSingleObject(hEnableReadSearch, INFINITE);// waiting for permission to read
 
 	DWORD byteRead;
 	if (!ReadFile(hReadPipe, &size1, sizeof(size1), &byteRead, NULL))
 	{
-		cout << "Array1 size read is failed " << endl;
+		std::cout << "Array1 size read is failed " << std::endl;
 		return GetLastError();
 	}
-	__int16* arr1 = new __int16[size1];
-	if (!ReadFile(hReadPipe, arr1, sizeof(__int16) * size1, &byteRead, NULL))
+	double* arr1 = new double[size1];
+	if (!ReadFile(hReadPipe, arr1, sizeof(double) * size1, &byteRead, NULL))
 	{
-		cout << "Array1 elements read is failed " << endl;
+		std::cout << "Array1 elements read is failed " << std::endl;
 		return GetLastError();
 	}
 	if (!ReadFile(hReadPipe, &size2, sizeof(size2), &byteRead, NULL))
 	{
-		cout << "Array2 size read is failed " << endl;
+		std::cout << "Array2 size read is failed " << std::endl;
 		return GetLastError();
 	}
-	__int8* arr2 = new __int8[size2];
-	if (!ReadFile(hReadPipe, arr2, sizeof(__int8) * size2, &byteRead, NULL))
+	double* arr2 = new double[size2];
+	if (!ReadFile(hReadPipe, arr2, sizeof(double) * size2, &byteRead, NULL))
 	{
-		cout << "Array2 elements read is failed " << endl;
+		std::cout << "Array2 elements read is failed " << std::endl;
 		return GetLastError();
 	}
-	cout << "Received information from Server:" << endl;
-	cout << "Size of array 1: " << size1 << endl;
-	cout << "Elements of array 1: ";
+	std::cout << "Received information from Server:" << std::endl;
+	std::cout << "Size of array 1: " << size1 << std::endl;
+	std::cout << "Elements of array 1: ";
 	for (int i = 0; i < size1; i++)
 	{
-		cout << arr1[i] << " ";
+		std::cout << arr1[i] << " ";
 	}
-	cout << endl;
-	cout << "Size of array 2: " << size2 << endl;
-	cout << "Elements of array 2: ";
+	std::cout << std::endl;
+	std::cout << "Size of array 2: " << size2 << std::endl;
+	std::cout << "Elements of array 2: ";
 	for (int i = 0; i < size2; i++)
 	{
-		cout << arr2[i] << " ";
+		std::cout << arr2[i] << " ";
 	}
-	cout << endl;
+	std::cout << std::endl;
 
-	set<double> uniqueElements(arr1, arr1 + size1);
-	vector<double> result;
+	std::set<double> uniqueElements(arr1, arr1 + size1);
+	std::vector<double> result;
 
 	for (int i = 0; i < size2; i++) {
 		if (uniqueElements.count(arr2[i]) > 0) {
@@ -77,28 +76,30 @@ int main(int argc, char* argv[]) {
 			result.push_back(un);
 	}
 	
-	cout << "Result array\n";
+	std::cout << "Result array\n";
 	for (int i = 0; i < result.size(); i++)
 	{
-		cout << result[i] << " ";
+		std::cout << result[i] << " ";
 	}
 
 	int resultSize = result.size();
 	if (!WriteFile(hWritePipe, &resultSize, sizeof(resultSize), &bytesWritten, NULL))
 	{
-		cout << "Result array size write is failed " << endl;
+		std::cout << "Result array size write is failed " << std::endl;
 		return GetLastError();
 	}
 	if (!WriteFile(hWritePipe, result.data(), resultSize * sizeof(double), &bytesWritten, NULL))
 	{
-		cout << "Result array elements write is failed " << endl;
+		std::cout << "Result array elements write is failed " << std::endl;
 		return GetLastError();
 	}
 	SetEvent(hEnableReadServer);
 
-	cout << "\nTo exit press any key ";
+	std::cout << "\nTo exit press any key ";
 	_getch();
 	
+	delete[] arr2;
+	delete[] arr1;
 	CloseHandle(hEnableReadSearch);
 	CloseHandle(hEnableReadServer);
 	CloseHandle(hWritePipe);
